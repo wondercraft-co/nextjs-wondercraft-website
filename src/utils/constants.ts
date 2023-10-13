b886645e35b870103a5cb988bc918803f0ca11c5
Juan Pablo Solano <hello@wondercraft.co>
2023-10-13 13:46:48 -0500

    updates

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const navigationItems = [
   { name: "Web Dev", href: "/services/web-development" },
-  { name: "Shopify Dev", href: "//shopify-development" },
+  { name: "Shopify Dev", href: "/services/shopify-development" },
   { name: "Wordpress Dev", href: "/services/wordpress-development" },
   { name: "About us", href: "/about-us" },
 ];
